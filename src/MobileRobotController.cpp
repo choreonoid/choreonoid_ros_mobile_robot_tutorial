73f480566f5d9372de8258b1d83b73c61ebca083
Shin'ichiro Nakaoka <s.nakaoka@gmail.com>
2023-08-18 20:37:52 +0900

    Arrange files

--- src/MobileRobotController.cpp
+++ src/MobileRobotController.cpp
@@ -1,75 +1,32 @@
 #include <cnoid/SimpleController>
-#include <ros/node_handle.h>
-#include <geometry_msgs/Twist.h>
-#include <mutex>
 
-using namespace std;
 using namespace cnoid;
-
 
 class MobileRobotController : public SimpleController
 {
-    std::unique_ptr<ros::NodeHandle> node;
-    ros::Subscriber subscriber;
-    geometry_msgs::Twist command;
-    std::mutex commandMutex;
     Link* wheels[2];
-    double dq_target[2];
 
     virtual bool initialize(SimpleControllerIO* io) override;
-    void commandCallback(const geometry_msgs::Twist& twist);
     virtual bool control() override;
 };
 
 CNOID_IMPLEMENT_SIMPLE_CONTROLLER_FACTORY(MobileRobotController)
 
-
 bool MobileRobotController::initialize(SimpleControllerIO* io)
 {
-    auto body = io->body();
-
-    wheels[0] = body->joint("RightWheel");
-    wheels[1] = body->joint("LeftWheel");
-
+    wheels[0] = io->body()->joint("RightWheel");
+    wheels[1] = io->body()->joint("LeftWheel");
     for(int i=0; i < 2; ++i){
         auto wheel = wheels[i];
         wheel->setActuationMode(JointTorque);
-        io->enableInput(wheel, JointVelocity);
         io->enableOutput(wheel, JointTorque);
     }
-
-    node = make_unique<ros::NodeHandle>(body->name());
-    subscriber = node->subscribe(
-        "/cmd_vel", 1, &MobileRobotController::commandCallback, this);
-    
     return true;
-}
-
-void MobileRobotController::commandCallback(const geometry_msgs::Twist& msg)
-{
-    std::lock_guard<std::mutex> lock(commandMutex);
-    command = msg;
 }
 
 bool MobileRobotController::control()
 {
-    constexpr double wheelRadius = 0.076;
-    constexpr double halfAxleWidth = 0.142;
-    constexpr double kdd = 5.0;
-    double dq_target[2];
-    
-    {
-        std::lock_guard<std::mutex> lock(commandMutex);
-        double dq_x = command.linear.x / wheelRadius;
-        double dq_yaw = command.angular.z * halfAxleWidth / wheelRadius;
-        dq_target[0] = dq_x - dq_yaw;
-        dq_target[1] = dq_x + dq_yaw;
-    }
-    
-    for(int i=0; i < 2; ++i){
-        auto wheel = wheels[i];
-        wheel->u() = kdd * (dq_target[i] - wheel->dq());
-    }
-    
+    wheel[0]->u() = 1.0;
+    wheel[0]->u() = -1.0;
     return true;
 }
