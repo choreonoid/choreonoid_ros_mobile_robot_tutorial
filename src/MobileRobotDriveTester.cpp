cb84a49784626b74585fc6850c806934f9ad98c9
Shin'ichiro Nakaoka <s.nakaoka@gmail.com>
2023-08-22 21:57:17 +0900

    Rename MobileRobotController to MobileRobotDriveTester

--- src/MobileRobotDriveTester.cpp
+++ src/MobileRobotDriveTester.cpp
@@ -1,6 +1,6 @@
 #include <cnoid/SimpleController>
 
-class MobileRobotController : public cnoid::SimpleController
+class MobileRobotDriveTester : public cnoid::SimpleController
 {
 public:
     virtual bool initialize(cnoid::SimpleControllerIO* io) override;
@@ -10,9 +10,9 @@ private:
     cnoid::Link* wheels[2];
 };
 
-CNOID_IMPLEMENT_SIMPLE_CONTROLLER_FACTORY(MobileRobotController)
+CNOID_IMPLEMENT_SIMPLE_CONTROLLER_FACTORY(MobileRobotDriveTester)
 
-bool MobileRobotController::initialize(cnoid::SimpleControllerIO* io)
+bool MobileRobotDriveTester::initialize(cnoid::SimpleControllerIO* io)
 {
     auto body = io->body();
     wheels[0] = body->joint("RightWheel");
@@ -25,7 +25,7 @@ bool MobileRobotController::initialize(cnoid::SimpleControllerIO* io)
     return true;
 }
 
-bool MobileRobotController::control()
+bool MobileRobotDriveTester::control()
 {
     wheels[0]->u() = 1.0;
     wheels[1]->u() = 1.0;
